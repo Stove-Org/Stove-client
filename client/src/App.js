fd4c766690a2505dd0c7903d77bac3709b2f288c
okdol1 <okdol0611@gmail.com>
2022-10-14 02:37:57 +0900

    Remove: Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import GlobalStyle from "./styles/GlobalStyle";
-import Router from "./router";
+
+import PageTemplate from "./components/oranisms/PageTemplate";
+import Home from "./pages/Home";
+import SignIn from "./pages/Auth/SignIn";
+import SignUp from "./pages/Auth/SignUp";
+import Setting from "./pages/Setting";
+import NextLCKRoster from "./pages/NextLCK/NextLCKRoster";
+import NextLCKResult from "./pages/NextLCK/NextLCKResult";
+import News from "./pages/News";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Router />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PageTemplate />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/setting" element={<Setting />} />
+            <Route path="/nextlck" element={<NextLCKRoster />} />
+            <Route path="/nextlck/:id" element={<NextLCKResult />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/search/:id" element={<Search />} />
+          </Route>
+
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
