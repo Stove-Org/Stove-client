4093f107dae0864e19c4921c8412384e7f83b111
okdol1 <okdol0611@gmail.com>
2022-10-15 20:01:59 +0900

    feat: NewsList component

--- client/src/App.js
+++ client/src/App.js
@@ -14,181 +14,14 @@ import NextLCKResult from "./pages/NextLCK/NextLCKResult";
 import News from "./pages/News";
 import Search from "./pages/Search";
 
-const DUMMY_DATA = [
-  {
-    id: 1,
-    nickName: "Faker",
-    name: "이상혁",
-    position: "MID",
-  },
-  {
-    id: 2,
-    nickName: "Chovy",
-    name: "정지훈",
-    position: "MID",
-  },
-  {
-    id: 3,
-    nickName: "Peanut",
-    name: "한왕호",
-    position: "JGL",
-  },
-  {
-    id: 4,
-    nickName: "Doran",
-    name: "최현준",
-    position: "TOP",
-  },
-  {
-    id: 5,
-    nickName: "Ruler",
-    name: "박재혁",
-    position: "BOT",
-  },
-  {
-    id: 6,
-    nickName: "Fate",
-    name: "유수혁",
-    position: "MID",
-  },
-  {
-    id: 7,
-    nickName: "Kiin",
-    name: "김기인",
-    position: "TOP",
-  },
-  {
-    id: 8,
-    nickName: "Pyosik",
-    name: "홍창현",
-    position: "JGL",
-  },
-  {
-    id: 9,
-    nickName: "Breyl",
-    name: "조건희",
-    position: "SPT",
-  },
-  {
-    id: 10,
-    nickName: "Zeka",
-    name: "김건우",
-    position: "MID",
-  },
-  {
-    id: 11,
-    nickName: "Nuguri",
-    name: "장하권",
-    position: "TOP",
-  },
-  {
-    id: 12,
-    nickName: "Canyon",
-    name: "김건부",
-    position: "JGL",
-  },
-  {
-    id: 13,
-    nickName: "Deokdam",
-    name: "서대길",
-    position: "BOT",
-  },
-];
+import ROSTER_DATA from "./data/ROSTER_DATA";
+import DUMMY_DATA from "./data/DUMMY_DATA";
+import NEWS_DATA from "./data/NEWS_DATA";
 
 function App() {
-  const [roster, setRoster] = useState([
-    {
-      id: 1,
-      name: "GEN",
-      TOP: "",
-      JGL: "",
-      MID: "Chovy",
-      BOT: "",
-      SPT: "",
-    },
-    {
-      id: 2,
-      name: "T1",
-      TOP: "Zeus",
-      JGL: "Oner",
-      MID: "",
-      BOT: "Gumayusi",
-      SPT: "Keria",
-    },
-    {
-      id: 3,
-      name: "LSB",
-      TOP: "",
-      JGL: "",
-      MID: "",
-      BOT: "",
-      SPT: "",
-    },
-    {
-      id: 4,
-      name: "DK",
-      TOP: "",
-      JGL: "",
-      MID: "",
-      BOT: "",
-      SPT: "",
-    },
-    {
-      id: 5,
-      name: "KT",
-      TOP: "",
-      JGL: "",
-      MID: "",
-      BOT: "",
-      SPT: "",
-    },
-    {
-      id: 6,
-      name: "DRX",
-      TOP: "",
-      JGL: "",
-      MID: "",
-      BOT: "",
-      SPT: "",
-    },
-    {
-      id: 7,
-      name: "KDF",
-      TOP: "",
-      JGL: "",
-      MID: "",
-      BOT: "",
-      SPT: "",
-    },
-    {
-      id: 8,
-      name: "NS",
-      TOP: "",
-      JGL: "",
-      MID: "",
-      BOT: "",
-      SPT: "",
-    },
-    {
-      id: 9,
-      name: "BRO",
-      TOP: "",
-      JGL: "",
-      MID: "",
-      BOT: "",
-      SPT: "",
-    },
-    {
-      id: 10,
-      name: "HLE",
-      TOP: "",
-      JGL: "",
-      MID: "",
-      BOT: "",
-      SPT: "",
-    },
-  ]);
+  const [roster, setRoster] = useState(ROSTER_DATA);
   const [players, setPlayers] = useState(DUMMY_DATA);
+  const [news, setNews] = useState(NEWS_DATA);
 
   return (
     <ThemeProvider theme={theme}>
@@ -212,7 +45,7 @@ function App() {
               path="/nextlck/:id"
               element={<NextLCKResult roster={roster} />}
             />
-            <Route path="/news" element={<News />} />
+            <Route path="/news" element={<News news={news} />} />
             <Route path="/search/:id" element={<Search />} />
           </Route>
 
--- client/src/pages/News/index.jsx
+++ client/src/pages/News/index.jsx
@@ -1,5 +1,36 @@
-const News = () => {
-  return <>News</>;
+import styled from "styled-components";
+
+import PageTitle from "../../components/atoms/PageTitle";
+import NewsList from "../../components/atoms/NewsList";
+
+const News = ({ news }) => {
+  return (
+    <>
+      <PageTitle title={"오늘의 HOT 뉴스"} />
+      <NewsWrapper>클릭수</NewsWrapper>
+
+      <PageTitle title={"최신 뉴스"} />
+      <NewsWrapper>
+        {news.map((el) => (
+          <NewsList
+            key={el.id}
+            newsUrl={el.newsUrl}
+            newsImg={el.newsImg}
+            headline={el.headline}
+            newsDate={el.newsDate}
+          />
+        ))}
+      </NewsWrapper>
+    </>
+  );
 };
+
+const NewsWrapper = styled.div`
+  padding: 20px 0;
+  margin-top: 20px;
+  border-color: ${(props) => props.theme.color.grayScale.gray20};
+  border-width: 1px 0 0;
+  border-style: solid;
+`;
 
 export default News;
