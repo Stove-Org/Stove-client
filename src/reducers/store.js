53018a1b6953fa92391ebd27197e5e503045b4b6
okdol1 <okdol0611@gmail.com>
2022-10-28 20:10:51 +0900

    feat: Redux-Persist 적용
    새로고침을 해도 데이터 유지

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,1 +1,26 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+
+import signinCheckSlice from "./signinCheckSlice";
+
+const reducers = combineReducers({
+  signinCheck: signinCheckSlice.reducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+
+export default store;
