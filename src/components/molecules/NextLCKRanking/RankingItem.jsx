a6ff5582d53143da626278cd5eeaaea3aa2e70a8
okdol1 <okdol0611@gmail.com>
2022-11-10 22:31:30 +0900

    feat: Ranking Component

--- src/components/molecules/NextLCKRanking/RankingItem.jsx
+++ src/components/molecules/NextLCKRanking/RankingItem.jsx
@@ -1,22 +1,79 @@
+import styled from "styled-components";
+
 const RankingItem = ({ item }) => {
-  console.log(item);
   return (
     <div>
       {item.progamer ? (
-        <>
-          <span>Top {item.ranking}</span>
-          <span>{item.progamer.nickname}</span>
-          <span>{item.percent.toFixed(1)}</span>
-          <img
-            scr={item.progamer.imgUrl}
+        <RankingItemWrapper>
+          <StyleNo>Top {item.ranking}</StyleNo>
+          <StyleNickname>{item.progamer.nickname}</StyleNickname>
+          <StylePercentWrapper>
+            <StylePercent percent={(item.percent * 100).toFixed(1)} />
+            <StylePercentText>
+              {(item.percent * 100).toFixed(1)}%
+            </StylePercentText>
+          </StylePercentWrapper>
+          <StyleImg
+            src={item.progamer.imgUrl}
             alt={`${item.progamer.nickname} 이미지`}
           />
-        </>
+        </RankingItemWrapper>
       ) : (
         <></>
       )}
     </div>
   );
 };
+
+const RankingItemWrapper = styled.div`
+  ${(props) => props.theme.typography.descriptionBold}
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  height: 48px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 1px 8px 0px;
+  border-radius: 3px;
+  background-color: ${(props) => props.theme.color.white};
+`;
+const StyleNo = styled.div`
+  color: ${(props) => props.theme.color.main100};
+  width: 70px;
+`;
+const StyleNickname = styled.div`
+  width: 100px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+const StylePercentWrapper = styled.div`
+  position: relative;
+  height: 20px;
+  margin-left: 10px;
+  background-color: ${(props) => props.theme.color.grayScale.gray20};
+  flex-grow: 1;
+  border-radius: 3px;
+`;
+const StylePercent = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: ${(props) => `${props.percent}%`};
+  height: 100%;
+  border-radius: 3px;
+  background-color: ${(props) => props.theme.color.main100};
+`;
+const StylePercentText = styled.div`
+  ${(props) => props.theme.typography.bodySmRegular}
+  position: absolute;
+  top: 2px;
+  left: 10px;
+  color: ${(props) => props.theme.color.white};
+`;
+const StyleImg = styled.img`
+  position: absolute;
+  bottom: 0;
+  right: 40px;
+  height: 56px;
+`;
 
 export default RankingItem;
--- src/components/molecules/NextLCKRanking/index.jsx
+++ src/components/molecules/NextLCKRanking/index.jsx
@@ -1,55 +1,162 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
+import Select from "react-select";
+import styled from "styled-components";
 import { getRanking } from "../../../api/next-lck";
 import RankingList from "./RankingList";
 
+import TOP from "../../../assets/svg/positions/TOP";
+import JGL from "../../../assets/svg/positions/JGL";
+import MID from "../../../assets/svg/positions/MID";
+import BOT from "../../../assets/svg/positions/BOT";
+import SPT from "../../../assets/svg/positions/SPT";
+
+const customStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: "1px solid #EEEEEE",
+    color: state.isSelected ? "#000000" : "#999999",
+    backgroundColor: "#ffffff",
+    "&:hover": {
+      backgroundColor: "#EEEEEE",
+      color: "#000000",
+    },
+    padding: "6px 16px",
+    fontFamily: "NotoSansKR-Regular",
+    fontSize: 16,
+  }),
+  control: () => ({
+    // none of react-select's styles are passed to <Control />
+    width: "100%",
+    backgroundColor: "#ffffff",
+    borderRadius: 3,
+    display: "flex",
+    boxShadow: "rgba(100, 100, 111, 0.2) 0px 1px 8px 0px",
+    padding: "6px 16px",
+    fontFamily: "NotoSansKR-Regular",
+    fontSize: 16,
+  }),
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+
+    return { ...provided, opacity };
+  },
+};
+
 const NextLCKRanking = () => {
+  const options = useMemo(
+    () => [
+      { value: "DRX", label: "DRX" },
+      { value: "T1", label: "T1" },
+      { value: "GEN", label: "Gen.G" },
+      { value: "DK", label: "DWG KIA" },
+      { value: "LSB", label: "Liiv SANDBOX" },
+      { value: "KT", label: "KT Rolster" },
+      { value: "KDF", label: "KWANGDONG FREECS" },
+      { value: "NS", label: "NongShim REDFORCE" },
+      { value: "BRO", label: "Fredit BRION" },
+      { value: "HLE", label: "Hanwha Life Esports" },
+    ],
+    []
+  );
+  const teamList = [
+    "DRX",
+    "T1",
+    "GEN",
+    "DK",
+    "LSB",
+    "KT",
+    "KDF",
+    "NS",
+    "BRO",
+    "HLE",
+  ];
   const [teamName, setTeamName] = useState("DRX");
   const [position, setPosition] = useState("TOP");
   const [data, setData] = useState([]);
+
   useEffect(() => {
     getRanking().then((res) => setData(res.data));
+    setTeamName(teamList[Math.random().toFixed(1) * 10]);
   }, []);
 
-  // setTimeout(() => {
-  //   if (position === "TOP") {
-  //     setPosition("JGL");
-  //   } else if (position === "JGL") {
-  //     setPosition("MID");
-  //   } else if (position === "MID") {
-  //     setPosition("BOT");
-  //   } else if (position === "BOT") {
-  //     setPosition("SPT");
-  //   } else if (position === "SPT") {
-  //     setPosition("TOP");
-  //   }
-  // }, 6000);
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      if (position === "TOP") {
+        setPosition("JGL");
+      } else if (position === "JGL") {
+        setPosition("MID");
+      } else if (position === "MID") {
+        setPosition("BOT");
+      } else if (position === "BOT") {
+        setPosition("SPT");
+      } else if (position === "SPT") {
+        setPosition("TOP");
+      }
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [position]);
+
   return (
     <>
       {data.length !== 0 ? (
-        <>
+        <RankingWrapper>
           <div>
-            <select name="team" onChange={(e) => setTeamName(e.target.value)}>
-              <option value="DRX">DRX</option>
-              <option value="T1">T1</option>
-              <option value="GEN">Gen.G</option>
-              <option value="DK">DWG KIA</option>
-              <option value="LSB">리브 샌드박스</option>
-              <option value="KT">KT Rolster</option>
-              <option value="KDF">광동 프릭스</option>
-              <option value="NS">농심 레드포스</option>
-              <option value="BRO">프레딧 브리온</option>
-              <option value="HLE">한화 e스포츠</option>
-            </select>
+            <Select
+              styles={customStyles}
+              options={options}
+              isSearchable={false}
+              defaultValue={() =>
+                options.find((item) => item.value === teamName)
+              }
+              onChange={(selected) => setTeamName(selected.value)}
+            />
           </div>
+          <ButtonsWrapper>
+            <TopButton position={position} onClick={() => setPosition("TOP")}>
+              <TOP
+                width={24}
+                height={24}
+                fill={position === "TOP" ? "#ffffff" : "#999999"}
+              />
+              <div>Top</div>
+            </TopButton>
+            <JGLButton position={position} onClick={() => setPosition("JGL")}>
+              <JGL
+                width={24}
+                height={24}
+                fill={position === "JGL" ? "#ffffff" : "#999999"}
+              />
+              <div>Junggle</div>
+            </JGLButton>
+            <MIDButton position={position} onClick={() => setPosition("MID")}>
+              <MID
+                width={24}
+                height={24}
+                fill={position === "MID" ? "#ffffff" : "#999999"}
+              />
+              <div>Mid</div>
+            </MIDButton>
+            <BOTButton position={position} onClick={() => setPosition("BOT")}>
+              <BOT
+                width={24}
+                height={24}
+                fill={position === "BOT" ? "#ffffff" : "#999999"}
+              />
+              <div>Bottom</div>
+            </BOTButton>
+            <SPTButton position={position} onClick={() => setPosition("SPT")}>
+              <SPT
+                width={24}
+                height={24}
+                fill={position === "SPT" ? "#ffffff" : "#999999"}
+              />
+              <div>Support</div>
+            </SPTButton>
+          </ButtonsWrapper>
           <div>
-            <button onClick={() => setPosition("TOP")}>Top</button>
-            <button onClick={() => setPosition("JGL")}>Junggle</button>
-            <button onClick={() => setPosition("MID")}>Mid</button>
-            <button onClick={() => setPosition("BOT")}>Bottom</button>
-            <button onClick={() => setPosition("SPT")}>Support</button>
-          </div>
-          <div>
-            <div>
+            <RankingListWrapper>
               {data
                 .filter(
                   (item) => item.team === teamName && item.position === position
@@ -57,14 +164,78 @@ const NextLCKRanking = () => {
                 .map((item, index) => {
                   return <RankingList item={item} key={index} />;
                 })}
-            </div>
+            </RankingListWrapper>
           </div>
-        </>
+        </RankingWrapper>
       ) : (
         <></>
       )}
     </>
   );
 };
+
+const RankingWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 2fr;
+  gap: 30px;
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const StyleButton = styled.button`
+  ${(props) => props.theme.typography.description}
+  padding: 12px 16px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 1px 8px 0px;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: #f54242;
+    color: #ffffff;
+  }
+  & + & {
+    margin-top: 14px;
+  }
+  & > div {
+    width: 90px;
+  }
+`;
+const TopButton = styled(StyleButton)`
+  background-color: ${(props) =>
+    props.position === "TOP" ? "#F54242" : "#ffffff"};
+  color: ${(props) => (props.position === "TOP" ? "#ffffff" : "#000000")};
+`;
+const JGLButton = styled(StyleButton)`
+  background-color: ${(props) =>
+    props.position === "JGL" ? "#F54242" : "#ffffff"};
+  color: ${(props) => (props.position === "JGL" ? "#ffffff" : "#000000")};
+`;
+const MIDButton = styled(StyleButton)`
+  background-color: ${(props) =>
+    props.position === "MID" ? "#F54242" : "#ffffff"};
+  color: ${(props) => (props.position === "MID" ? "#ffffff" : "#000000")};
+`;
+const BOTButton = styled(StyleButton)`
+  background-color: ${(props) =>
+    props.position === "BOT" ? "#F54242" : "#ffffff"};
+  color: ${(props) => (props.position === "BOT" ? "#ffffff" : "#000000")};
+`;
+const SPTButton = styled(StyleButton)`
+  background-color: ${(props) =>
+    props.position === "SPT" ? "#F54242" : "#ffffff"};
+  color: ${(props) => (props.position === "SPT" ? "#ffffff" : "#000000")};
+`;
+
+const RankingListWrapper = styled.div`
+  & > div + div {
+    margin-top: 14px;
+  }
+`;
 
 export default NextLCKRanking;
